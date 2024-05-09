7203d2181d5d671b6109d301c7f4b23b9c1101b7
Derrickragui <87313913+Derrickragui@users.noreply.github.com>
2024-05-09 15:27:34 +0300

    main

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns= [
-    path('login/', views.login, name= 'login'),
-    path('confirmlogin/', views.otphandler, name= 'confirmlogin'), 
-    path('redirectuser/', views.redirectuser, name= 'redirectuser')
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('confirmlogin/', views.otphandler, name='confirmlogin'),
+    path('redirectuser/', views.redirectuser, name='redirectuser')
 ]
